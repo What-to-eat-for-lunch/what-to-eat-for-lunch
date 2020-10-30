0cb7d09ea773112f74803724d8694d440d1f7d36
HRim-dev <gpfla46771@daum.net>
2020-10-30 18:15:20 +0900

    룰렛 수정(다시돌리기 이벤트 수정 전)

--- frontend/src/pages/roulette.js
+++ frontend/src/pages/roulette.js
@@ -1,63 +1,17 @@
 //룰렛 화면
 import React,{ Component } from 'react';
+import '../components/css/roulette.css';
+import {TweenLite } from 'gsap';
+import { Sine } from '../../node_modules/gsap/gsap-core';
+import { Linear } from '../../node_modules/gsap/gsap-core';
+import {confirmAlert} from 'react-confirm-alert';
 import $ from 'jquery';
 import jQuery from "jquery";
-window.$ = window.jQuery = jQuery
+window.$ = window.jQuery = jQuery;
 
 
-
-class roulette extends Component{
-    
-    componentDidMount(){
-        var gift;
-        var  present =[ 1,2,3 , 4,5,6 ]
-    
-        function iniGame(num){
-            gift = num;
-            TweenLite.killTweensOf($(".board_on"));
-            TweenLite.to($(".board_on"), 0, {css:{rotation:rotationPos[gift]}});
-            TweenLite.from($(".board_on"),5, {css:{rotation:-3000}, onComplete:endGame, ease:Sine.easeOut});
-                   console.log("gift 숫자 : "+ (gift +1) +"rotationPos" + rotationPos );
-        }
-    
-        var rotationPos = new Array(60,120,180,240,300,360);
-    
-        TweenLite.to($(".board_on"), 360, {css:{rotation:-4000}, ease: Linear.easeNone});
-        function endGame(){
-                  var  copImg= "http://img.babathe.com/upload/specialDisplay/htmlImage/2019/test/coupon"+( gift +1) + ".png";
-                    console.log("이미지 : " + copImg );
-                  $('#result').text("결과:"+copImg);
-    
-                  var str='<input type="button" id="btn" value="맛집 추천 지도"/>';
-                  $('#mapButton').html(str);
-     
-        }
-   
-    
-    
-    $(function() {
-        var clicked  =0;
-        for(i=1; i<7; i++){
-        // 상품쪽 이미지는 신경 안쓰셔도 됩니다!! 책임님!!!
-          var  pictures = "http://img.babathe.com/upload/specialDisplay/htmlImage/2019/test/coupon"+ i  + ".png";
-          $(".board_on").append('<img  src="' + pictures + '" />');
-        }
-    
-        $(".join").on("mousedown",function(){
-          if( clicked <= 0){    iniGame(Math.floor(Math.random() *6));    }
-          else  if( clicked >=1 ){   
-              if(confirm('다시 추천받으시겠습니까?')){
-                iniGame(Math.floor(Math.random() *6));
-                $('#mapButton').append();
-            }else{
-                event.preventDefault();
-            }
-         }
-          clicked ++
-        });
-    });
-    }
-
+class roulette extends Component{  
+    //컴포넌트를 DOM에 부착(렌더링)
     render(){
         return(
             <div id="contents">
@@ -76,6 +30,64 @@ class roulette extends Component{
             </div>
         );
     }
+
+    //렌더링 완료 후 실행되는 메소드
+    componentDidMount(){
+      var data;
+
+      function iniGame(num){//룰렛 초기
+         data=num;
+        
+         //TweenLite:룰렛애니메이션 효과를 주기위해 사용
+         TweenLite.killTweensOf($(".board_on"));//특정 트윈 죽이기
+         TweenLite.to($(".board_on"),0,{css:{rotation:rotationPos[data]}});//애니메이션의 최종지점
+         TweenLite.from($(".board_on"),5,{css:{rotation:-3000},onComplete:endGame, ease:Sine.easeOut});//애니메이션의 시작지점(회전을 위해 Sine 사용)
+         console.log("data 숫자:"+(data+1)+"rotationPos"+rotationPos);
+      };
+
+      var rotationPos = new Array(60,120,180,240,300,360);
+
+      TweenLite.to($(".board_on"), 360, {css:{rotation:-4000}, ease: Linear.easeNone});//움직임 없애기
+
+      function endGame(){
+       var  copImg= "http://img.babathe.com/upload/specialDisplay/htmlImage/2019/test/coupon"+( data +1) + ".png";
+         console.log("이미지 : " + copImg );
+       $('#result').text("결과:"+copImg);
+
+       var str='<input type="button" id="btn" value="맛집 추천 지도"/>';
+       $('#mapButton').html(str);
+      };
+
+      $(function() {
+       var clicked  =0;
+       for(var i=1; i<7; i++){
+       // 상품쪽 이미지는 신경 안쓰셔도 됩니다!! 책임님!!!
+         var  pictures = "http://img.babathe.com/upload/specialDisplay/htmlImage/2019/test/coupon"+ i  + ".png";
+         $(".board_on").append('<img  src="' + pictures + '" />');
+       }
+   
+       $(".join").on("mousedown",function(){
+         if( clicked <= 0){    iniGame(Math.floor(Math.random() *6));    }
+         else  if( clicked >=1 ){   
+           confirmAlert({
+              title:'다시 돌리기',
+              message:'다시 추천받으시겠습니까?',
+              buttons:[
+                {
+                  label:'예',
+                  onClick:()=>{iniGame(Math.floor(Math.random() *6));
+                  $('#mapButton').append();}
+                },{
+                 label:'아니오',
+                 onClick:()=>{this.preventDefault();}
+               },
+             ]
+            });
+           }
+         clicked ++
+       });
+   });
+ }
 }
 
 export default roulette;

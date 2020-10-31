b6715f9e4401df53303d0c4543cf14fca52f598f
HRim-dev <gpfla46771@daum.net>
2020-10-31 11:27:36 +0900

    장소 검색 가능한 지도로 수정
    검색버튼 누르면 정해진 키워드로 장소 마커로 표시

--- frontend/src/pages/map.js
+++ frontend/src/pages/map.js
@@ -1,3 +1,4 @@
+/* global kakao */
 import React, { useState, useEffect } from 'react'
 import Header from '../header/header';
 import Footer from '../footer/footer';
@@ -24,51 +25,83 @@ const KakaoMap = () => {
     setLocationArr(dataJSON.location)
   }
 
+  //지도 생성
   const createMap = () => {
-    const script = document.createElement('script')
-    script.async = true
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&autoload=false`
-    document.head.appendChild(script)
-    script.onload = () => {
+    const script1 = document.createElement('script')
+    script1.async = true
+    script1.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&autoload=false`
+    document.head.appendChild(script1)
+    script1.onload = () => {
       const { kakao } = window
       kakao.maps.load(() => {
         let container = document.getElementById('Mymap')
         let options = {
           center: new kakao.maps.LatLng(37.506502, 127.053617),
-          level: 7,
+          level: 3,
         }
         const createdMap = new kakao.maps.Map(container, options)
-        setMap(createdMap)
+        setMap(createdMap)       
       })
     }
+
+    const script2 = document.createElement('script')
+    script2.async = true
+    script2.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&libraries=services&autoload=false`
+    document.head.appendChild(script2)
+
+    const script3 = document.createElement('script')
+    script3.async = true
+    script3.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${APP_KEY}&libraries=services,clusterer,drawing&autoload=false`
+    document.head.appendChild(script3)
+
   }
 
-  const createMarker = () => {
+  //장소 검색 객체 생성
+  const createps=()=>{
+      const { kakao } = window
+      //장소 검색 객체 생성
+      var ps = new kakao.maps.services.Places(); 
+      //키워드로 장소 검색 객체 생성
+      ps.keywordSearch('이태원 맛집', placesSearchCB);
+  }
+
+  //맛집위치에 마커 표시
+  function displayMarker(place){
     const { kakao } = window
-    const tempArr = []
-    locationArr.forEach(e => {
-      tempArr.push(
-        new kakao.maps.Marker({
-          map: map,
-          position: new kakao.maps.LatLng(e.mapY, e.mapX),
-        })
-      )
-    })
-    setMarkerArr(tempArr)
-  }
+     // 마커를 생성하고 지도에 표시합니다
+     var marker = new kakao.maps.Marker({
+      map: map,
+      position: new kakao.maps.LatLng(place.y, place.x) 
+  });
+  // 마커를 클릭하면 장소명을 표출할 인포윈도우 입니다
+  var infowindow = new kakao.maps.InfoWindow({zIndex:1});
+  // 마커에 클릭이벤트를 등록합니다
+  kakao.maps.event.addListener(marker, 'click', function() {
+      // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+      infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+      infowindow.open(map, marker);
+  });
+}
+ 
+  //키워드 검색 완료시 호출되는 콜백함수
+  function placesSearchCB (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
 
-  const deleteMarker = () => markerArr.forEach(e => e.setMap(null))
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        var bounds = new kakao.maps.LatLngBounds();
+        map.setCenter(new kakao.maps.LatLng(data[0].y, data[0].x));
 
-  useEffect(() => {
-    getLocation(1) // location 정보 fetch
-    createMap()
-  }, [])
-
-  // marker 생성 + 표시
-  useEffect(() => map && locationArr.length && createMarker(), [
-    map,
-    locationArr,
-  ])
+        for (var i=0; i<data.length; i++) {
+            displayMarker(data[i]);    
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+      }
+    }
+    useEffect(() => {
+      getLocation(1) // location 정보 fetch
+      createMap()    
+    }, [])
 
   return (
     <div className="App">
@@ -80,6 +113,7 @@ const KakaoMap = () => {
                 <button class="previous_step">이전단계로 돌아가기</button>
         </Link>
         <h2>맛집 추천 지도</h2>
+        <button class="right" onClick={createps}>검색</button>
       
       <div id="Mymap" style={{ width: '100vw', height: '100vh' }}></div>
     </div>
